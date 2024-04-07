d6e81d0b5691d5e54f054c748967537f6b84a875
natanjmai <natan.mai@hotmail.com>
2024-04-07 21:10:55 +0930

    Seeds file and layout changes
    * Seeds with Product list
    * Cursor pointer and other style changes.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,282 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+Product.create_or_find_by(name: 'Gin Believer',
+                          organization_id: 1,
+                          volume: 0.750,
+                          prep_method: '',
+                          best_before: nil,
+                          status: true,
+                          price_cents: 6500,
+                          unit: 'liter')
+
+Product.create_or_find_by(name: 'Vermute de Caju com botânicos',
+                          organization_id: 1,
+                          volume: 0.7,
+                          prep_method: '',
+                          best_before: nil,
+                          status: true,
+                          price_cents: 13400,
+                          unit: 'liter')
+
+Product.create_or_find_by(name: 'Tônica',
+                          organization_id: 1,
+                          volume: 1,
+                          prep_method: '',
+                          best_before: nil,
+                          status: true,
+                          price_cents: 400,
+                          unit: 'liter')
+
+Product.create_or_find_by(name: 'Suco de Limão',
+                          organization_id: 1,
+                          volume: 1,
+                          prep_method: '',
+                          best_before: nil,
+                          status: true,
+                          price_cents: 700,
+                          unit: 'liter')
+
+Product.create_or_find_by(name: 'Xarope de Açúcar',
+                          organization_id: 1,
+                          volume: 1,
+                          prep_method: '',
+                          best_before: nil,
+                          status: true,
+                          price_cents: 400,
+                          unit: 'liter')
+
+Product.create_or_find_by(name: 'Absolut',
+                          organization_id: 1,
+                          volume: 1,
+                          prep_method: '',
+                          best_before: nil,
+                          status: true,
+                          price_cents: 8600,
+                          unit: 'liter')
+
+Product.create_or_find_by(name: 'Limao Taiti',
+                          organization_id: 1,
+                          volume: 1,
+                          prep_method: '',
+                          best_before: nil,
+                          status: true,
+                          price_cents: 700,
+                          unit: 'kg')
+
+Product.create_or_find_by(name: 'Amarula',
+                          organization_id: 1,
+                          volume: 0.750,
+                          prep_method: '',
+                          best_before: nil,
+                          status: true,
+                          price_cents: 9600,
+                          unit: 'liter')
+
+Product.create_or_find_by(name: 'Licor 43',
+                          organization_id: 1,
+                          volume: 0.700,
+                          prep_method: '',
+                          best_before: nil,
+                          status: true,
+                          price_cents: 17600,
+                          unit: 'liter')
+
+Product.create_or_find_by(name: 'Cafe Capuccino',
+                          organization_id: 1,
+                          volume: 1,
+                          prep_method: '',
+                          best_before: nil,
+                          status: true,
+                          price_cents: 800,
+                          unit: 'und')
+
+Product.create_or_find_by(name: 'Licor Fino',
+                          organization_id: 1,
+                          volume: 1,
+                          prep_method: '',
+                          best_before: nil,
+                          status: true,
+                          price_cents: 8130,
+                          unit: 'liter')
+
+Product.create_or_find_by(name: 'Havana 7',
+                          organization_id: 1,
+                          volume: 0.7,
+                          prep_method: '',
+                          best_before: nil,
+                          status: true,
+                          price_cents: 13600,
+                          unit: 'liter')
+
+Product.create_or_find_by(name: 'Gengibre',
+                          organization_id: 1,
+                          volume: 1,
+                          prep_method: '',
+                          best_before: nil,
+                          status: true,
+                          price_cents: 600,
+                          unit: 'kg')
+
+Product.create_or_find_by(name: 'Suco de Abacaxi',
+                          organization_id: 1,
+                          volume: 1,
+                          prep_method: '',
+                          best_before: nil,
+                          status: true,
+                          price_cents: 600,
+                          unit: 'liter')
+
+Product.create_or_find_by(name: 'Suco de Laranja',
+                          organization_id: 1,
+                          volume: 1,
+                          prep_method: '',
+                          best_before: nil,
+                          status: true,
+                          price_cents: 600,
+                          unit: 'liter')
+
+Product.create_or_find_by(name: 'Amaro',
+                          organization_id: 1,
+                          volume: 0.75,
+                          prep_method: '',
+                          best_before: nil,
+                          status: true,
+                          price_cents: 6000,
+                          unit: 'liter')
+
+Product.create_or_find_by(name: 'Licor de Cerveja',
+                          organization_id: 1,
+                          volume: 0.75,
+                          prep_method: '',
+                          best_before: nil,
+                          status: true,
+                          price_cents: 9200,
+                          unit: 'liter')
+
+Product.create_or_find_by(name: 'Licor de Avelã',
+                          organization_id: 1,
+                          volume: 0.75,
+                          prep_method: '',
+                          best_before: nil,
+                          status: true,
+                          price_cents: 16990,
+                          unit: 'liter')
+
+Product.create_or_find_by(name: 'Whisky Bourbon',
+                          organization_id: 1,
+                          volume: 1,
+                          prep_method: '',
+                          best_before: nil,
+                          status: true,
+                          price_cents: 10000,
+                          unit: 'liter')
+
+Product.create_or_find_by(name: 'Purê Pêssego Monin',
+                          organization_id: 1,
+                          volume: 1,
+                          prep_method: '',
+                          best_before: nil,
+                          status: true,
+                          price_cents: 8800,
+                          unit: 'liter')
+
+Product.create_or_find_by(name: 'Chá capim limão',
+                          organization_id: 1,
+                          volume: 1,
+                          prep_method: '',
+                          best_before: nil,
+                          status: true,
+                          price_cents: 500,
+                          unit: 'liter')
+
+Product.create_or_find_by(name: 'Gin Believer com Clitoria Ternatea',
+                          organization_id: 1,
+                          volume: 0.7,
+                          prep_method: '',
+                          best_before: nil,
+                          status: true,
+                          price_cents: 7000,
+                          unit: 'liter')
+
+Product.create_or_find_by(name: 'Xarope de Lichia',
+                          organization_id: 1,
+                          volume: 0.7,
+                          prep_method: '',
+                          best_before: nil,
+                          status: true,
+                          price_cents: 4900,
+                          unit: 'liter')
+
+Product.create_or_find_by(name: 'Rum Branco',
+                          organization_id: 1,
+                          volume: 0.75,
+                          prep_method: '',
+                          best_before: nil,
+                          status: true,
+                          price_cents: 7600,
+                          unit: 'liter')
+
+Product.create_or_find_by(name: 'Rum Escuro',
+                          organization_id: 1,
+                          volume: 0.75,
+                          prep_method: '',
+                          best_before: nil,
+                          status: true,
+                          price_cents: 13600,
+                          unit: 'liter')
+
+Product.create_or_find_by(name: 'Campari',
+                          organization_id: 1,
+                          volume: 0.9,
+                          prep_method: '',
+                          best_before: nil,
+                          status: true,
+                          price_cents: 5100,
+                          unit: 'liter')
+
+Product.create_or_find_by(name: 'Xarope de Morango',
+                          organization_id: 1,
+                          volume: 0.7,
+                          prep_method: '',
+                          best_before: nil,
+                          status: true,
+                          price_cents: 4900,
+                          unit: 'liter')
+
+Product.create_or_find_by(name: 'Licor de Creme de Cassis ou Frutas Vermelhas',
+                          organization_id: 1,
+                          volume: 0.75,
+                          prep_method: '',
+                          best_before: nil,
+                          status: true,
+                          price_cents: 5976,
+                          unit: 'liter')
+
+Product.create_or_find_by(name: 'Xarope de Gengibre',
+                          organization_id: 1,
+                          volume: 0.70,
+                          prep_method: '',
+                          best_before: nil,
+                          status: true,
+                          price_cents: 4900,
+                          unit: 'liter')
+
+Product.create_or_find_by(name: 'Folhas de Hortelã',
+                          organization_id: 1,
+                          volume: 30,
+                          prep_method: '',
+                          best_before: nil,
+                          status: true,
+                          price_cents: 200,
+                          unit: 'und')
+
+Product.create_or_find_by(name: 'Vermute Rosso',
+                          organization_id: 1,
+                          volume: 0.750,
+                          prep_method: '',
+                          best_before: nil,
+                          status: true,
+                          price_cents: 3300,
+                          unit: 'liter')
