5a1521b0e7300f6ded54db4700dc2926a2320b7f
natanjmai <natan.mai@hotmail.com>
2024-06-06 23:52:47 +0930

    Start refactoring project

--- app/javascript/jigger.js
+++ app/javascript/jigger.js
@@ -1,67 +1,67 @@
 // Function to set up event listeners
 function setupEventListeners() {
-    document.addEventListener('change', function(event) {
-        if (event.target && event.target.matches('.inline-datasheet-price')) {
-            // Get the input element within the class .inline-datasheet-price
-            let inputElement = document.querySelector('input.inline-datasheet-price');
-            let icon = $('.datasheet-price-refresh');
+  document.addEventListener('change', function(event) {
+    if (event.target && event.target.matches('.inline-datasheet-price')) {
+      // Get the input element within the class .inline-datasheet-price
+      let inputElement = document.querySelector('input.inline-datasheet-price');
+      let icon = $('.datasheet-price-refresh');
 
-            if (inputElement) {
-                // Get the value of the input element
-                let value = inputElement.value;
-                let url = inputElement.getAttribute('data-url');
-                let element = $('.datasheet-cmv');
+      if (inputElement) {
+        // Get the value of the input element
+        let value = inputElement.value;
+        let url = inputElement.getAttribute('data-url');
+        let element = $('.datasheet-cmv');
 
-                if (value) {
-                    icon.addClass('fa-spin');
+        if (value) {
+          icon.addClass('fa-spin');
 
-                    setTimeout(() => {
-                        $.ajax({
-                            url: url,
-                            method: 'GET',
-                            dataType: 'JSON',
-                            data: {
-                                customer_price: value
-                            },
-                            success: function(response) {
-                                // Remove others cmv classes
-                                element.removeClass (function (index, className) {
-                                    return (className.match(/(^|\s)cmv-\S+/g) || []).join(' ');
-                                });
+          setTimeout(() => {
+            $.ajax({
+              url: url,
+              method: 'GET',
+              dataType: 'JSON',
+              data: {
+                customer_price: value
+              },
+              success: function(response) {
+                // Remove others cmv classes
+                element.removeClass (function (index, className) {
+                  return (className.match(/(^|\s)cmv-\S+/g) || []).join(' ');
+                });
 
-                                element.addClass(response['cmv_class']);
-                                element.text(response['cmv'] + '%');
-                            },
-                            error: function(xhr, status, error) {
-                            }
-                        });
+                element.addClass(response['cmv_class']);
+                element.text(response['cmv'] + '%');
+              },
+              error: function(xhr, status, error) {
+              }
+            });
 
-                        icon.removeClass('fa-spin');
-                    }, 500);
-                }
-            }
+            icon.removeClass('fa-spin');
+          }, 500);
         }
+      }
+    }
+  });
+
+  // Vertical menu toggle
+  document.addEventListener('turbo:load', () => {
+    $('#sidebarCollapse').on('click', function() {
+      $('#sidebar, #content, #alert-div, #alert-msg').toggleClass('active');
     });
 
-    // Vertical menu toggle
-    document.addEventListener('turbo:load', () => {
-        $('#sidebarCollapse').on('click', function() {
-            $('#sidebar, #content, #alert-div, #alert-msg').toggleClass('active');
-        });
+  });
 
-    });
-
-    $('.datasheet-price-refresh').on('click', function() {
-        location.reload();
-    });
+  $('.datasheet-price-refresh').on('click', function() {
+    location.reload();
+  });
 }
 
 // Call the setup function on initial page load
 document.addEventListener('DOMContentLoaded', function() {
-    setupEventListeners();
+  setupEventListeners();
 });
 
 // Reapply event listeners after Turbo Streams actions
 document.addEventListener('turbo:load', function() {
-    setupEventListeners();
+  setupEventListeners();
 });
